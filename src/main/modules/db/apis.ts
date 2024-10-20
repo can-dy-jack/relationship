6f48b55e6d402f48714d27ea0f0774280b2f74d8
can-dy-jack <jjack_chen3@163.com>
2024-10-20 14:15:09 +0800

    再次更换模板 - electron-react-boilerplate

--- src/main/modules/db/apis.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from './database'
-
-export async function test() {
-  return await prisma.collection.findMany()
-}
--- src/main/modules/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { ipcMain } from 'electron'
-import { test } from './apis'
-
-export default function initApis() {
-  ipcMain.on(`apis:test`, async (event) => {
-    console.log(1, event)
-    event.returnValue = await test()
-  })
-}
